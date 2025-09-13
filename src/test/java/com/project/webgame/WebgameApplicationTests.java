41891fc2dd767cc1c4e911d8fcffd5fb1068652a
daydreaam <dayingyeo@naver.com>
2025-09-13 12:02:12 +0900

    init project

---
+++ src/test/java/com/project/webgame/WebgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
